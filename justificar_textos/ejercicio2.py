ac7b3c9879921e486a80bd47cc0f8d9557b00798
PaoloVeliz <paoloveliz@gmail.com>
2022-11-05 16:02:16 -0600

    casi listo

--- justificar_textos/ejercicio2.py
+++ justificar_textos/ejercicio2.py
@@ -8,23 +8,31 @@ def justificar_textos(cadena, maximo):
         for i in solo_palabras: 
             letras_por_palabra = letras_por_palabra + len(i)
         espacios_faltantes = maximo - letras_por_palabra
-        espacios = math.ceil(espacios_faltantes / len(solo_palabras))
-        print(espacios_faltantes / len(solo_palabras))
-        print(espacios)
-        nueva_cadena = ''
-        for i in range(0,len(solo_palabras)):
-            if i != len(solo_palabras) - 1:
-                nueva_cadena = nueva_cadena
-                espacios_a_agregar = ''
-                for conteo in range(0,espacios):
-                    espacios_a_agregar = espacios_a_agregar + ' ' 
-                solo_palabras[i] = solo_palabras[i] + espacios_a_agregar
-                nueva_cadena = nueva_cadena + solo_palabras[i]
-            else:
-                nueva_cadena = nueva_cadena + solo_palabras[i]
-        print(nueva_cadena)
+        espacios = espacios_faltantes / len(solo_palabras)
+        if espacios.is_integer():
+            pass
+        else:
+            # print(espacios_faltantes)
+            # print(espacios_faltantes / len(solo_palabras))
+            # print(espacios)
+            espacios = int(espacios)
+            nueva_cadena = ''
+            for i in range(0,len(solo_palabras)):
+                if i != len(solo_palabras) - 1:
+                    nueva_cadena = nueva_cadena
+                    espacios_a_agregar = ''
+                    for conteo in range(0,espacios):
+                        espacios_a_agregar = espacios_a_agregar + ' ' 
+                    solo_palabras[i] = solo_palabras[i] + espacios_a_agregar
+                    nueva_cadena = nueva_cadena + solo_palabras[i]
+                else:
+                    for conteo in range(1,len(solo_palabras)):
+                        espacios_a_agregar = espacios_a_agregar + ' ' 
+                    nueva_cadena = nueva_cadena +  espacios_a_agregar + solo_palabras[i]
+            print(nueva_cadena)
+            # print(len(nueva_cadena))
 
 
 
     
-justificar_textos(["hola amigos mundo","malos dias"], 20)
+justificar_textos(["hola amigos mundo","malos dias","hola"], 20)
